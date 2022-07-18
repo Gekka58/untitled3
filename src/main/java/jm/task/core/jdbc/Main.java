65a49fea479905166cf583fa3dfefb90d394b437
Gekka58 <He588@yandex.ru>
2022-07-18 21:59:11 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        System.out.println("aaa"); // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Nikolai", "Safronov", (byte) 12);
+        userService.saveUser("Alexei", "Golovanov", (byte) 45);
+        userService.saveUser("Veronika", "Milohina", (byte) 35);
+        userService.saveUser("Aleksandr", "Vorobiev", (byte) 22);
+        userService.getAllUsers();
+        userService.removeUserById(2);
+        userService.cleanUsersTable();
+        userService.dropUsersTable(); // реализуйте алгоритм здесь
     }
 }
